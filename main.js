0f2c6106214f964222f7b4f53235ad53644239cb
melissaleigh1993 <60311554+melissaleigh1993@users.noreply.github.com>
2020-08-27 11:22:32 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+$(function() {
+//set Interval
+setInterval(function() {$('#slider .slides').animate({'margin-left': "-=720px'}, 1000);
+}, 3000);
+//animate margin-left
+//if last slide, go to position 1 (0px)
+
+//listen for mouseover and pause
+//resume on mouseleave
+});
