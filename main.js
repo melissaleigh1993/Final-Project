1fe52fa4c0882cefd7cbb40829c6db1e2ef6ac23
melissaleigh1993 <60311554+melissaleigh1993@users.noreply.github.com>
2020-08-27 11:46:22 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,43 @@
 'use strict';
 
 $(function() {
-//set Interval
-setInterval(function() {$('#slider .slides').animate({'margin-left': "-=720px'}, 1000);
-}, 3000);
+
+//configuration
+var width = 720;
+var animationSpeed = 1000;
+var pause = 3000;
+  var currentSlide = 1;
+  
+ //cache DOM
+  var $slider = $('#slider');
+  var $slideContainer = $slider.find('.slides');
+  var $slides = $slideContainer.find('.slide');
+  
+  var interval;
+  
+    //setInterval
+  function startSlider(){
+interval = setInterval(function() {
+  $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+  currentSlide++;
+    if(currentSlide === $slides.length) {
+     currentSlide = 1;
+      $slideContainer.css('margin-left', 0);
+    }
+  });
+}, pause);
+  }
+  
+  function stopSlider() {
+    clearInterval(interval);
+  }
+  
 //animate margin-left
 //if last slide, go to position 1 (0px)
 
 //listen for mouseover and pause
+  $slider.on('mouseenter', stopSlider).on('mouseleave', startSlider);
+  
+  startSlider();
 //resume on mouseleave
 });
